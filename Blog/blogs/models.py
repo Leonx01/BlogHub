74df73041c3905ad24b649e3919e87b00ec25c26
nileonx <nileonx@gmail.com>
2023-07-09 02:44:53 +0800

    基本完成了用户权限管理和功能完善，将在下一版本改进UI设计

--- Blog/blogs/models.py
+++ Blog/blogs/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         if len(self.text)<50:
--- Blog/blogs/views.py
+++ Blog/blogs/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 from blogs.models import BlogPost
 from blogs.forms import PostForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     return render(request, 'blogs/index.html')
+
 def posts(request):
     posts = BlogPost.objects.order_by('date_added')
     context = {'posts': posts}
     return render(request, 'blogs/posts.html', context)
+
 def post(request,post_id):
     post = BlogPost.objects.get(id=post_id)
     context = {'post': post}
     return render(request, 'blogs/post.html', context)
+@login_required
 def edit_post(request,post_id):
     post = BlogPost.objects.get(id=post_id)
+    if request.user!=post.author:
+        raise Http404
     if request.method != 'POST':
         form = PostForm(instance=post)
     else:
@@ -25,14 +31,16 @@ def edit_post(request,post_id):
             return HttpResponseRedirect(reverse('blogs:post',args=[post.id]))
     context = {'post':post,'form':form}
     return render(request,'blogs/edit_post.html',context)
-
+@login_required
 def new_post(request):
     if request.method != 'POST':
         form = PostForm()
     else:
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_post=form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
             return HttpResponseRedirect(reverse('blogs:posts'))
     context = {'form':form}
     return render(request,'blogs/new_post.html',context)
